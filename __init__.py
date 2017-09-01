a7a5e75b8c7bc83771abb7743638067eda84798f
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-09-01 15:31:20 -0500

    Change skill to use Padatious

--- __init__.py
+++ __init__.py
@@ -58,18 +58,8 @@ class TimeSkill(MycroftSkill):
             return "%I:%M, %p"
 
     def initialize(self):
-        self.__build_speak_intent()
-        self.__build_display_intent()
-
-    def __build_speak_intent(self):
-        intent = IntentBuilder("SpeakIntent").require("QueryKeyword") \
-            .require("TimeKeyword").optionally("Location").build()
-        self.register_intent(intent, self.handle_speak_intent)
-
-    def __build_display_intent(self):
-        intent = IntentBuilder("DisplayIntent").require("DisplayKeyword") \
-            .require("TimeKeyword").optionally("Location").build()
-        self.register_intent(intent, self.handle_display_intent)
+        self.register_intent_file('ask.time.intent', self.handle_ask_time)
+        self.register_intent_file('show.time.intent', self.handle_show_time)
 
     def get_timezone(self, locale):
         try:
@@ -83,7 +73,7 @@ class TimeSkill(MycroftSkill):
                 return None
 
     def get_time(self):
-        location = self.message.data.get("Location")  # optional parameter
+        location = self.message.data.get("location")  # optional parameter
         nowUTC = datetime.datetime.now(timezone('UTC'))
         tz = self.get_timezone(self.location_timezone)
 
@@ -152,14 +142,14 @@ class TimeSkill(MycroftSkill):
                 self.display(current_time)
             self.timer.start()
 
-    def handle_speak_intent(self, message):
+    def handle_ask_time(self, message):
         self.message = message  # optional parameter
         self.isClockRunning = True
         self.speak_dialog("time.current", {"time": self.get_time()})
         if sum_of_core >= compatible_core_version_sum:
             self._update_time()
 
-    def handle_display_intent(self, message):
+    def handle_show_time(self, message):
         self.message = message
         self.isClockRunning = True
         if sum_of_core >= compatible_core_version_sum:
